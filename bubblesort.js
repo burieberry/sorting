f71f28ca24c5c4159cfec058e687668ab1de6598
Burcu Noyan <burcunoyan@gmail.com>
2017-07-08 20:04:10 -0400

    adjust spacing

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,9 @@
 function bubbleSort(arr) {
-  for(var j = 0; j<arr.length; j++){
+  for(var j = 0; j < arr.length; j++) {
     for (var i = 1; i < arr.length; i++) {
-      if (arr[i-1] >= arr[i]) {
-        var temp = arr[i-1];
-        arr[i-1] = arr[i];
+      if (arr[i - 1] >= arr[i]) {
+        var temp = arr[i - 1];
+        arr[i - 1] = arr[i];
         arr[i] = temp;
       }
     }
