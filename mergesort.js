0de28fce1f8eb2c5a7202ad5e71ebdcdcf107d20
Burcu Noyan <burcunoyan@gmail.com>
2018-02-12 16:01:18 -0500

    write Merge tests and function

--- mergesort.js
+++ mergesort.js
@@ -18,3 +18,34 @@ function split(arr) {
 
   return [firstHalf, secondHalf];
 }
+
+function merge(arr1, arr2) {
+  let i = 0, j = 0;
+  let newArr = [];
+
+  for (var k = 0; i < arr1.length && j < arr2.length; k++) {
+    if (arr1[i] <= arr2[j]) {
+      newArr[k] = arr1[i];
+      i++;
+    }
+
+    else {
+      newArr[k] = arr2[j];
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    newArr[k] = arr1[i];
+    k++;
+    i++;
+  }
+
+  while (j < arr2.length) {
+    newArr[k] = arr2[j];
+    k++;
+    j++;
+  }
+
+  return newArr;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -5,3 +5,11 @@ describe('Split function', () => {
     expect(split([-3, 5, -3, 0, 0])).toEqual([[-3, 5, -3], [0, 0]]);
   });
 });
+
+
+describe('Merge function', () => {
+  it('can merge two sorted arrays into one sorted array', () => {
+    expect(merge([1, 2, 5], [3, 4, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(merge([1, 8], [-5, -3, 0])).toEqual([-5, -3, 0, 1, 8]);
+  });
+});
