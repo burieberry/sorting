0976a97562186daa140bf3498173376452dc266b
Burcu Noyan <burcunoyan@gmail.com>
2018-02-12 17:16:31 -0500

    use window object in specs

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,14 @@
-const sorting = {};
-
-sorting.bubbleSort = function(arr) {
+function bubbleSort(arr) {
   for (var i = 0; i < arr.length - 1; i++) {
     if (arr[i] > arr[i + 1]) {
-      this.swap(arr, i, i + 1);
-      this.bubbleSort(arr);
+      swap(arr, i, i + 1);
+      bubbleSort(arr);
     }
   }
   return arr;
 }
 
-sorting.swap = function (arr, firstIndex, secondIndex) {
+function swap(arr, firstIndex, secondIndex) {
   let temp = arr[firstIndex];
   arr[firstIndex] = arr[secondIndex];
   arr[secondIndex] = temp;
